aef604fbd37b3fcc1ef7a8db743825b97d3a2efc
kebudke <31251507+kebudke@users.noreply.github.com>
2017-12-03 20:51:16 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,6 +8,18 @@ app = Flask(__name__)
 def render_main():
         return render_template('home.html')
 
+@app.route("/bytitle")
+def render_t1():
+        return render_template('tab1.html')
+
+@app.route("/bygenre")
+def render_t2():
+        return render_template('tab2.html')
+
+@app.route("/bylevel")
+def render_t3():
+        return render_template('tab3.html')
+
 if __name__=="__main__":
     app.run(debug=False)
 
